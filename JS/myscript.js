e37f06add428c16aac0fd2796130426bc5ad7997
Georgia Demiri <georgiademiri@gmail.com>
2018-11-07 19:20:57 +0200

    Files

---
+++ JS/myscript.js
@@ -0,0 +1,8 @@
+var theButton = document.getElementById("helloButton");
+   theButton.onclick = function ()
+   {
+   var theName = document.getElementById("yourName").value;
+  document.getElementById("viewName").innerHTML = theName;
+var theLocation = document.getElementById("yourLocation").value
+document.getElementById("viewLocation").innerHTML = theLocation;}
+  
